54d1c30c243650357feeb443b08356ead277fe25
Nick Weiland <njweiland23@gmail.com>
2023-09-28 12:14:29 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const mainNode = document.getElementById('main')
+if (mainNode) {
+    mainNode.parentNode.removeChild(mainNode)
+}
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.textContent = "Nick Weiland is the champion!"
